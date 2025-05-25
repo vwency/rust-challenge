8b302f7fbd7e38b75ecd08dd619befa665748026
vwency <duckdodo666@gmail.com>
2025-05-25 20:50:15 +0600

    chore: update deps & methods names & count of transfer

--- src/generator/address.rs
+++ src/generator/address.rs
@@ -1,7 +1,7 @@
 use rand::Rng;
 
 pub fn rand_address<R: Rng + ?Sized>(rng: &mut R) -> String {
-    let bytes: [u8; 20] = rng.gen();
+    let bytes: [u8; 20] = rng.random();
 
     let hex_str = bytes.iter()
         .map(|b| format!("{:02x}", b))
--- src/generator/transfer.rs
+++ src/generator/transfer.rs
@@ -1,6 +1,5 @@
 use anyhow::{Context, Result};
 use rand::Rng;
-use rand::prelude::ThreadRng;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::model::Transfer;
@@ -23,7 +22,7 @@ impl DefaultTransferGenerator {
 
 impl TransferGenerator for DefaultTransferGenerator {
     fn generate(&self, count: usize) -> Result<Vec<Transfer>> {
-        let mut rng: ThreadRng = rand::thread_rng();
+        let mut rng = rand::rng();
 
         let now = SystemTime::now()
             .duration_since(UNIX_EPOCH)
@@ -34,12 +33,14 @@ impl TransferGenerator for DefaultTransferGenerator {
             .map(|_| {
                 let from = rand_address(&mut rng);
                 let to = rand_address(&mut rng);
-                let amount = rng.gen_range(self.config.min_amount..self.config.max_amount);
-                let usd_price = rng.gen_range(self.config.min_price..self.config.max_price);
+
+                let amount = rng.random_range(self.config.min_amount..self.config.max_amount);
+                let usd_price = rng.random_range(self.config.min_price..self.config.max_price);
+
                 let ts = if self.config.max_age_secs == 0 {
                     now
                 } else {
-                    now - rng.gen_range(0..self.config.max_age_secs)
+                    now - rng.random_range(0..self.config.max_age_secs)
                 };
 
                 Transfer {
